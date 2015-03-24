1943240812c70648f05e4617dd9cf9f76b0fbfcd
Kader Sucuk <kadersk@gmail.com>
2015-03-24 17:44:02 +0200

    Rerender posts list is done.

--- app/assets/javascripts/posts.js.jsx
+++ app/assets/javascripts/posts.js.jsx
@@ -34,16 +34,68 @@ var PostEl = React.createClass({
   }
 });
 
-var Posts = React.createClass({
+var NewPost = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = this.refs.title.getDOMNode().value.trim();
+    var content = this.refs.content.getDOMNode().value.trim();
+
+    this.refs.title.getDOMNode().value = '';
+    this.refs.content.getDOMNode().value = '';
+    $.ajax({
+      url: "/posts.json",
+      dataType: "json",
+      type: "POST",
+      data: {post: {title: title, content: content}},
+      success: function(data) {
+        this.props.onNewPost(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log("error creating post", xhr, status, err);
+      }.bind(this)
+    });
+  },
   render: function() {
-    var posts = this.props.posts;
-    console.log(posts);
     return (
-      <ul class="posts">
-        {posts.map(function(post){
-          return <PostEl title={post.title} content={post.content} />
-        })}
-      </ul>
+      <form class="new-post" onSubmit={this.handleSubmit}>
+        <div class="form-group">
+          <label for="title">Title</label>
+
+          <input type="text" ref="title" id="title" class="form-control" name="title" placeholder="title" />
+        </div>
+        <div class="form-group">
+          <label for="content">Content</label>
+          <textarea name="content" ref="content" id="content" class="form-control" placeholder="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    )
+  }
+});
+
+var Posts = React.createClass({
+  getInitialState: function() {
+    return {posts: this.props.posts}
+  },
+  addPost: function(post) {
+    var updatedPosts = this.state.posts.slice();
+    updatedPosts.push(post);
+    this.setState({posts: updatedPosts});
+  },
+  render: function() {
+    console.log("rendering", this.props.posts);
+    var posts = this.props.posts;
+    return (
+      <div>
+        <ul class="posts">
+          {this.state.posts.map(function(post){
+            return <PostEl title={post.title} content={post.content} />
+          })}
+        </ul>
+        <div class="post-form">
+          <NewPost onNewPost={this.addPost}/>
+        </div>
+      </div>
     )
   }
 });
